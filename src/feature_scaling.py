249f22347ff55a3dc4be3220e33ff89039003056
Naduka Yomal <naduka898@gmail.com>
2026-07-04 01:46:51 +0530

    feat: Add scaling file here

---
+++ src/feature_scaling.py
@@ -0,0 +1,31 @@
+import os
+import logging
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from abc import ABC, abstractmethod , anstractmethod 
+from typing import List
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+import warnings
+
+
+class ScalingStrategy(ABC):
+    @abstractmethod
+    def scale(self, df, columns):
+        pass
+
+
+class StandardScaler(ScalingStrategy):
+    def __init__(self):
+        self.scaler = StandardScaler()
+        self.fitted = False
+
+    def scale(self, df : pd.DataFrame, columns : List):
+        df[columns] = self.scaler.fit_transform(df[columns])
+        self.fitted = True
+        logging.INFO(f"Applied Standard Scaling for {columns} features")
+        return df
+
+    def get_scaler(self):
+        return self.scaler
