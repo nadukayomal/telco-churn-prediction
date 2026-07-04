9ed778a5b7855cd019e898b8b342f912c5494061
Naduka Yomal <naduka898@gmail.com>
2026-07-04 23:53:04 +0530

    feat: Add the model training file

---
+++ src/model_training.py
@@ -0,0 +1,25 @@
+import os
+import logging
+import joblib
+import pandas as pd
+import numpy as np
+from typing import Str, List
+
+logging.basicConfig(
+                        level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s'
+                    )
+
+
+class ModelTrainer:
+    def train(self, model, X_train, Y_train):
+        model.fit(X_train, Y_train)
+        train_score = model.score(X_train, Y_train)
+        return model, train_score
+
+    def save_model(self, model, file_path):
+        joblib.dump(model, file_path)
+        logging.INFO(f"Save model {file_path} file path")
+
+    def load_model(self, file_path):
+        return joblib.load(file_path)
