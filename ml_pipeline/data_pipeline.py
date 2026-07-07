301660ec7ce9ba940ab7a045ab16e2975cca0fbf
Naduka Yomal <naduka898@gmail.com>
2026-07-07 13:33:32 +0530

    feat : develop data ingestion pipeline

--- ml_pipeline/data_pipeline.py
+++ ml_pipeline/data_pipeline.py
@@ -21,6 +21,7 @@ import logging
 import json
 import pandas as pd
 import numpy as np
+from typing import Dict, Optional
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "utils"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
 
@@ -32,4 +33,52 @@ from feature_encoding import NominalEncodingStrategy, OrdialEncodingStrategy
 from feature_scaling import StandardScaler
 from data_splitter import SimpleTrainTestSplitStrategy
 from imbalanced_handle import SmoteImbalanceHander
-from config import get_path, get_preprocessing, get_logging
+from config import get_path, get_preprocessing, get_logging, get_columns
+
+
+
+def build_data_pipeline (
+                        data_path : str = "data/raw/Telco-Customer-Churn.csv", 
+                        target_column : str = "Churn", 
+                        test_size : float = 0.2, 
+                        force_rebuild : bool = False
+                        ):
+
+    data_path_config = get_path()
+    data_preprocess_config = get_preprocessing()
+    columns_config = get_columns()
+
+    """Data Ingestions part"""
+
+    print("Data ingestions process")
+    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    raw_data_path = data_path_config.get("data", {}).get("raw", {}) or data_path
+
+    if isinstance(raw_data_path, str) and not os.path.isabs(raw_data_path):
+        raw_data_path = os.path.join(root_dir, raw_data_path)
+    
+    #  Set the path where processed data exist 
+    processed_data_dir = os.path.join(
+                                        os.path.dirname(__file__),
+                                        "..",
+                                        data_path_config.get("data", {}).get("processed_dir")
+                                    )
+
+
+
+
+
+
+    """ Bellow are remove later only for validate """
+    # print("Every compoent has on preprocesing path")
+    # for key, val in data_preprocess_config.items():
+    #     print(key)
+    # print("\nEvery compoent has on data path")
+    # for key, val in data_path_config.items():
+    #     print(key)
+    # print("\nEvery compoent has on columns")
+    # for key, val in columns_config.items():
+    #     print(key)
+    # print(raw_data_path)
+
+build_data_pipeline()
