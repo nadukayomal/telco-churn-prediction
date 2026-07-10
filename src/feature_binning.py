bccf09c7872a13aa752852f6cf83b174545e8bcd
Naduka Yomal <naduka898@gmail.com>
2026-07-10 10:20:17 +0530

    fix : fix the bug has on binning file

--- src/feature_binning.py
+++ src/feature_binning.py
@@ -13,33 +13,33 @@ class BinningStrategy(ABC):
 
 
 class CustomBinningStrategy(BinningStrategy):
-    def __init__(self, binnig_definition):
-        self.binnig_definition = binnig_definition
+    def __init__(self, binning_definition):
+        self.binning_definition = binning_definition
 
     def bin_feature(self, column, df):
         # may double check the function
         def assign_bin(value):
-            for bin_label, bin_range in self.binnig_definition.items():
+            for bin_label, bin_range in self.binning_definition.items():
                 if bin_range[0 ]<= value < bin_range[1]:
                     return bin_label
             return "invalied"
 
         df[f"{column}Bins"] = df[column].apply(assign_bin)
         del df[column]
-        logging.INFO(f"Applied custom bin for {column} and create new column {column}Bins")
+        logging.info(f"Applied custom bin for {column} and create new column {column}Bins")
         return df
 
 class BinaryBinnig(BinningStrategy):
-    def __init__(self, binnig_definition):
-        self.binning_definition = binnig_definition
+    def __init__(self, binning_definition):
+        self.binning_definition = binning_definition
 
     def bin_feature(self, column: str, df: pd.DataFrame):
         def assign_bin(value):
             for label , values in self.binning_definition.items():
                 if value in values:
                     return label
-            return "Invalied"
 
         df[f"{column}"] = df[column].apply(assign_bin)
-        logging.INFO(f"Applied binary binning for {column}")
+        logging.info(f"Applied binary binning for {column}")
+        return df
         
